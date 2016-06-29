69e62a7c49b62e4d48ec03e490804f02ace6cc6d
Evangelos Rokas <evrokas@gmail.com>
2016-06-29 12:53:37 +0300

    * Added DEBUG_SLEEP_CYCLE compiler define in order to enable/suppress   debug messages while power cycling * Moved doAction() function in the end. Probably we do not need it at all! * Removed loop1().

--- main.ino
+++ main.ino
@@ -66,7 +66,7 @@ void setup()
 	
 
 
-	powerPeripherals( (unsigned char)0, 0 );	/* disable all peripherals */
+	powerPeripherals(0,0);	/* disable all peripherals */
 	Serial.begin( 9600 );
 	Dln("\n\n\nHello world. Sleep cycling begins!");
 	sprintf(tmp, "Sleep duration %d sec, cycles %d", SLEEP_CYCLE, maxSleepCycle);
@@ -81,6 +81,100 @@ void setup()
 	setTime(0, 29, 2, 0, 21, 6, 16);
 	powerPeripherals( 0, 0 );
 #endif
+}
+
+
+void mySleep()
+{
+	Serial.flush();
+	LowPower.powerDown(LP_SLEEP_MODE, ADC_OFF, BOD_OFF);
+}
+
+
+
+void loop()
+{
+  datetime_t dt;
+  float f1, f2;
+  unsigned long f3a, f3b;
+
+
+	/* get initial time */
+    powerPeripherals(1,1);
+    getTime(&dt);
+    powerPeripherals(0,0);
+    
+    lastMinLogCycle = getMinutes(&dt);
+    curMinLogCycle = lastMinLogCycle;
+
+
+	/* enter endless loop */
+  	while(1) {
+  		mySleep();
+	
+  		curSleepCycle++;
+
+#define DEBUG_SLEEP_CYCLE	1
+		
+#ifdef DEBUG_SLEEP_CYCLE
+  		D("curSleepCycle ");Dln(curSleepCycle);
+#endif
+
+		if(curSleepCycle >= maxSleepCycle) {
+		    /* do not reset curSleepCycle yet, since PERIOD might not
+		     * have been reached yet, do it inside the inner loop */
+	
+#ifdef DEBUG_SLEEP_CYCLE
+			D("curSleepCycle >= maxSleepCycle ");Dln(curSleepCycle);
+#endif			
+
+			f3a = readVcc();
+			powerPeripherals(1, 1);
+			getTime(&dt);
+            powerPeripherals(0, 0);
+
+			curMinLogCycle = getMinutes(&dt);
+			if(curMinLogCycle - lastMinLogCycle >= maxMinLogCycle) {
+
+#ifdef DEBUG_SLEEP_CYCLE
+			    D("curMinLogCycle - lastMinLogCycle >= MaxMinLogCycle ");
+			    D(curMinLogCycle); D(" "); Dln(lastMinLogCycle);
+#endif
+
+			    lastMinLogCycle = curMinLogCycle;
+
+			    curSleepCycle = 0;	/* reset sleep cycle */
+			  
+			    powerPeripherals(1,1);
+    			f1 = dht.getTemperature();
+    			f2 = dht.getHumidity();
+    			f3b = readVcc();
+  	    		powerPeripherals(0, 0);
+			
+                Serial.print(">>,");
+		    	/* date */
+		    	convertDate2Str(tmp, &dt);
+		    	Serial.print(tmp); Serial.print(",");
+			
+		    	/* time */
+		    	convertTime2Str(tmp, &dt);
+		    	Serial.print(tmp); Serial.print(",");
+			
+		    	/* power voltage, before powering peripherals */
+		    	Serial.print(f3a); Serial.print(",");
+			
+				/* power voltage, after powering peripherals */
+		    	Serial.print(f3b); Serial.print(",");
+			
+		    	/* temperature */
+		    	Serial.print(f1); Serial.print(",");
+			
+		    	Serial.print(f2); Serial.print("\n");
+
+		    	Serial.flush();
+            }
+		}
+	}
 }
 
 void doActions(char doLog, char doNet)
@@ -104,139 +198,4 @@ void doActions(char doLog, char doNet)
 	/* shutdown peripherals */
 	if(doLog || doNet)
 		powerPeripherals( (unsigned char)0, 2 );
-}
-
-void mySleep()
-{
-//	D("sleep... ");
-	Serial.flush();
-	LowPower.powerDown(LP_SLEEP_MODE, ADC_OFF, BOD_OFF);
-//	Serial.begin( 9600 );
-//	Dln("wakeup!");
-}
-
-
-
-void loop()
-{
-  datetime_t dt;
-  float f1, f2;
-  unsigned long f3a, f3b;
-
-
-
-    powerPeripherals(1,1);
-    getTime(&dt);
-    powerPeripherals(0,0);
-    
-    lastMinLogCycle = getMinutes(&dt);
-    curMinLogCycle = lastMinLogCycle;
-  
-
-  
-  	while(1) {
-  		mySleep();
-	
-  		curSleepCycle++;
-  		D("curSleepCycle ");Dln(curSleepCycle);
-
-		if(curSleepCycle >= maxSleepCycle) {
-		    /* do not reset curSleepCycle yet, since PERIOD might not
-		     * have been reached yet, do it inside the inner loop */
-	
-		    D("curSleepCycle >= maxSleepCycle ");Dln(curSleepCycle);
-			f3a = readVcc();
-			powerPeripherals(1, 1);
-			getTime(&dt);
-            powerPeripherals(0, 0);
-
-			curMinLogCycle = getMinutes(&dt);
-			if(curMinLogCycle - lastMinLogCycle >= maxMinLogCycle) {
-
-			    D("curMinLogCycle - lastMinLogCycle >= MaxMinLogCycle ");
-			    D(curMinLogCycle); D(" "); Dln(lastMinLogCycle);
-			    lastMinLogCycle = curMinLogCycle;
-
-			    curSleepCycle = 0;	/* reset sleep cycle */
-			  
-			    powerPeripherals(1,1);
-    			f1 = dht.getTemperature();
-    			f2 = dht.getHumidity();
-    			f3b = readVcc();
-  	    		powerPeripherals(0, 0);
-			
-                Serial.print(">>,");
-		    	/* date */
-		    	convertDate2Str(tmp, &dt);
-		    	Serial.print(tmp); Serial.print(",");
-			
-		    	/* time */
-		    	convertTime2Str(tmp, &dt);
-		    	Serial.print(tmp); Serial.print(",");
-			
-		    	/* power voltage */
-		    	Serial.print(f3a); Serial.print(",");
-			
-		    	Serial.print(f3b); Serial.print(",");
-			
-		    	/* temperature */
-		    	Serial.print(f1); Serial.print(",");
-			
-		    	Serial.print(f2); Serial.print("\n");
-
-		    	Serial.flush();
-            }
-		}
-	}
-}
-
-
-
-/* main loop of the project */
-void loop1()
-{
-  char doLog=0, doNet=0;
-
-
-
-  	/* prepare to sleep and sleep */
-  	D("Current cycle : (log) "); D(doLog); D("  (net) "); Dln(doNet);
-  	D("About to sleep... ");
-  	LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-  	
-  	
-  	
-  	/* wake up! */
-  	curSleepCycle++;
-  	if(curSleepCycle >= maxSleepCycle) {
-  		/* we should check real-time clock if we are ready to log or do net communication */
-  		
-  		powerRTC( 1, 2 );	/* powerup RTC module, and delay for 2 msec to stabilize */
-
-        }
-
-
-  		
-  
-  if(curLogCycle >= maxLogCycle) {
-    doLog=1;
-    curLogCycle = 0;
-  }
-  
-  if(curNetCycle >= maxNetCycle) {
-    doNet=1;
-    curNetCycle = 0;
-  }    
-
-  D("Current cycle : (log) "); D(doLog); D("  (net) "); Dln(doNet);
-  
-  doActions(doLog, doNet);
-
-  D("About to sleep... ");
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-
-  /* so! we woke up again */
-  Dln("wake up!");
-  curLogCycle++;
-  curNetCycle++;
 }
