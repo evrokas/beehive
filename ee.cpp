76f66230551785561a23bc7b0438931b624f3f7b
Evangelos Rokas <evrokas@gmail.com>
2017-09-11 17:44:58 +0300

    * added ee.cpp

---
+++ ee.cpp
@@ -0,0 +1,39 @@
+#ifndef __EE_H__
+#define __EE_H__
+
+#if LINUX_NATIVE_APP == 1
+
+
+#else
+
+#include <Arduino.h>
+#include <Wire.h>
+
+#endif
+
+
+#include "mem.h"
+#include "data.h"
+
+
+extern uint8_t		__ee_dev_addr;
+extern uint32_t	__ee_dev_size;
+
+
+#if LINUX_NATIVE_APP == 1
+
+/* compile for linux native application, used for testing
+ * functions */
+
+#include "drivers/ee_linux.hpp"
+
+#else
+
+/* compile for beehive production board
+ */
+
+#include "../drivers/ee_i2c.hpp"
+
+#endif	/* LINUX_NATIVE_APP */
+
+#endif	/* __EE_H__ */
